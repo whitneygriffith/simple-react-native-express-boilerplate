ecaaeafe9b0609d7f254702d7defd47d613f6a15
Whitney Griffith <whitney.griffith@bison.howard.edu>
2019-01-16 15:16:33 +0200

    README.md

--- client/App.js
+++ client/App.js
@@ -2,34 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-
-  state = {
-    data: null
-  };
-
-  componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
-
-
-
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.data}</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
